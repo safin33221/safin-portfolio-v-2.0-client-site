84c11dca9c2a3e159f0feaacd5d8483f7858fb4d
safin33221 <safin33221@gmail.com>
2025-10-12 22:36:27 +0600

    Fixed dashboard Sticky

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -1,16 +1,20 @@
-import Sidebar from '@/components/shared/Sidebar';
-import React from 'react';
+import Sidebar from "@/components/shared/Sidebar";
+import React from "react";
 
-const dashboardLayout = ({ children }: { children: React.ReactNode }) => {
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
     return (
-        <div className='max-w-[1920px] flex   mx-auto'>
-            <Sidebar />
-            <main className=' w-full min-md:ml-40'>
+        <div className="max-w-[1920px] flex mx-auto min-h-screen">
+            {/* Sticky Sidebar */}
+            <div className="sticky top-0 h-screen">
+                <Sidebar />
+            </div>
 
-            {children}
+            {/* Main Content */}
+            <main className="flex-1 overflow-y-auto p-4 md:p-8 bg-[#0f001a]/80 text-white">
+                {children}
             </main>
         </div>
     );
 };
 
-export default dashboardLayout;
+export default DashboardLayout;
